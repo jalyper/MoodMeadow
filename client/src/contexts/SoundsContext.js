1169cc6c248c41d67192aae777e5dcd56d51b9e8
Jalyper <keaton.r.anderson@gmail.com>
2024-01-12 19:47:16 -0700

    added full name for sound bucket

--- client/src/contexts/SoundsContext.js
+++ client/src/contexts/SoundsContext.js
@@ -8,11 +8,11 @@ export const SoundsProvider = ({ children }) => {
 
   useEffect(() => {
     setSounds([
-      { id: 1, name: 'Light Rain', src: `https://${process.env.SOUND_BUCKET}/moodmeadow-light-rain-30-min.wav` },
-      { id: 2, name: 'Soft Sketch', src: `https://${process.env.SOUND_BUCKET}/moodmeadow-Soft-Sketch.wav` },
-      { id: 3, name: 'Light Rain, Drone Bass (C)', src: `https://${process.env.SOUND_BUCKET}/moodmeadow-Light-Rain_Drone-Bass_C.wav` },
-      { id: 4, name: 'Soft Drone Bass (C)', src: `https://${process.env.SOUND_BUCKET}/moodmeadow-Soft-Drone-Bass_C.wav` },
-      { id: 5, name: 'Soft Flute (C)', src: `https://${process.env.SOUND_BUCKET}/moodmeadow-Soft-Flute_C.wav` },
+      { id: 1, name: 'Light Rain', src: `https://${process.env.REACT_APP_SOUND_BUCKET}/moodmeadow-light-rain-30-min.wav` },
+      { id: 2, name: 'Soft Sketch', src: `https://${process.env.REACT_APP_SOUND_BUCKET}/moodmeadow-Soft-Sketch.wav` },
+      { id: 3, name: 'Light Rain, Drone Bass (C)', src: `https://${process.env.REACT_APP_SOUND_BUCKET}/moodmeadow-Light-Rain_Drone-Bass_C.wav` },
+      { id: 4, name: 'Soft Drone Bass (C)', src: `https://${process.env.REACT_APP_SOUND_BUCKET}/moodmeadow-Soft-Drone-Bass_C.wav` },
+      { id: 5, name: 'Soft Flute (C)', src: `https://${process.env.REACT_APP_SOUND_BUCKET}/moodmeadow-Soft-Flute_C.wav` },
       // ... other sounds
     ]);
   }, []);
