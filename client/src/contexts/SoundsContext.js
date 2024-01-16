dc9fc262f32eb1ce00b282467a211975983c41c1
Jalyper <keaton.r.anderson@gmail.com>
2024-01-16 15:37:38 -0700

    changed sound file urls on SoundsContext

--- client/src/contexts/SoundsContext.js
+++ client/src/contexts/SoundsContext.js
@@ -8,11 +8,11 @@ export const SoundsProvider = ({ children }) => {
 
   useEffect(() => {
     setSounds([
-      { id: 1, name: 'Light Rain', src: 'light-rain-30-min.wav' },
-      { id: 2, name: 'Soft Sketch', src: 'moodmeadow-Soft-Sketch.wav' },
-      { id: 3, name: 'Light Rain, Drone Bass (C)', src: 'moodmeadow-Light-Rain_Drone-Bass_C.wav' },
-      { id: 4, name: 'Soft Drone Bass (C)', src: 'moodmeadow-Soft-Drone-Bass_C.wav' },
-      { id: 5, name: 'Soft Flute (C)', src: 'moodmeadow-Soft-Flute_C.wav' },
+      { id: 1, name: 'Light Rain', src: process.env.REACT_APP_SOUND_BUCKET + 'light-rain-30-min.wav' },
+      { id: 2, name: 'Soft Sketch', src: process.env.REACT_APP_SOUND_BUCKET + 'Soft-Sketch.wav' },
+      { id: 3, name: 'Light Rain, Drone Bass (C)', src: process.env.REACT_APP_SOUND_BUCKET + 'Light-Rain_Drone-Bass_C.wav' },
+      { id: 4, name: 'Soft Drone Bass (C)', src: process.env.REACT_APP_SOUND_BUCKET + 'Soft-Drone-Bass_C.wav' },
+      { id: 5, name: 'Soft Flute (C)', src: process.env.REACT_APP_SOUND_BUCKET + 'Soft-Flute_C.wav' },
       // ... other sounds
     ]);
   }, []);
