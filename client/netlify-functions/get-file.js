42e514e2399eb9b5064b5ad6c515d2a10c91e3cc
Jalyper <keaton.r.anderson@gmail.com>
2024-01-16 19:36:09 -0700

    added logging to get-file

--- client/netlify-functions/get-file.js
+++ client/netlify-functions/get-file.js
@@ -10,6 +10,8 @@ AWS.config.update({
 const s3 = new AWS.S3({ region: 'us-east-1' });
 
 exports.handler = async function(event, context) {
+    console.log('Function called with event:', event);
+
     context.callbackWaitsForEmptyEventLoop = false;
 
     const headers = {
@@ -20,6 +22,7 @@ exports.handler = async function(event, context) {
 
     // Handle OPTIONS method for CORS preflight
     if (event.httpMethod === 'OPTIONS') {
+        console.log('OPTIONS method received');
         return { 
             statusCode: 204, 
             headers: headers
@@ -27,6 +30,7 @@ exports.handler = async function(event, context) {
     }
 
     if (event.httpMethod !== 'GET') {
+        console.log('Non-GET method received:', event.httpMethod);
         return { 
             statusCode: 405, 
             body: 'Method Not Allowed',
@@ -36,6 +40,7 @@ exports.handler = async function(event, context) {
 
     // Check if the Authorization header exists
     if (!event.headers.authorization) {
+        console.log('No Authorization header');
         return { 
             statusCode: 401, 
             body: 'No Authorization header',
@@ -45,26 +50,37 @@ exports.handler = async function(event, context) {
 
     // Extract the JWT from the Authorization header
     const token = event.headers.authorization.split(' ')[1];
+    console.log('Token:', token);
 
     try {
         // Validate the JWT
+        console.log('Validating JWT...');
         const decoded = await new Promise((resolve, reject) => {
             jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-                if (err) reject(err);
-                else resolve(decoded);
+                if (err) {
+                    console.log('Error validating JWT:', err);
+                    reject(err);
+                }
+                else {
+                    console.log('JWT validated:', decoded);
+                    resolve(decoded);
+                }
             });
         });
 
         // Extract the file key from the path
         const pathParts = event.path.split('/');
         const fileKey = pathParts[3];
-        console.log('fileKey: ' + fileKey);
+        console.log('File key:', fileKey);
+
         // Generate a pre-signed URL for the file
+        console.log('Generating pre-signed URL...');
         const url = s3.getSignedUrl('getObject', {
             Bucket: process.env.REACT_APP_SOUND_BUCKET,
             Key: fileKey,
             Expires: 60 * 5 // URL will be valid for 5 minutes
         });
+        console.log('Pre-signed URL:', url);
 
         return {
             statusCode: 200,
@@ -72,6 +88,7 @@ exports.handler = async function(event, context) {
             headers: headers
         };
     } catch (err) {
+        console.log('An error occurred:', err);
         return { 
             statusCode: 500, 
             body: 'An error occurred',
