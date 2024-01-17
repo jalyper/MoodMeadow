14f4fbf3600824d93fb1b63d10c52a0a8ba52d7c
Jalyper <keaton.r.anderson@gmail.com>
2024-01-16 20:25:04 -0700

    added logging to debug file key being named incorrectly

--- client/netlify-functions/get-file.js
+++ client/netlify-functions/get-file.js
@@ -70,6 +70,7 @@ exports.handler = async function(event, context) {
 
         // Extract the file key from the path
         const pathParts = event.path.split('/');
+        console.log('Path parts:', pathParts);
         const fileKey = pathParts[3];
         console.log('File key:', fileKey);
 
