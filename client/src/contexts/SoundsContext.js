3c6cd12facd16d66f21dff5cd7f87c909a04616e
Jalyper <keaton.r.anderson@gmail.com>
2024-01-23 10:01:23 -0700

    fixed sound name typo

--- client/src/contexts/SoundsContext.js
+++ client/src/contexts/SoundsContext.js
@@ -10,7 +10,7 @@ export const SoundsProvider = ({ children }) => {
   useEffect(() => {
     setSounds([
       { id: 1, name: 'Light Rain', src: 'light-rain-30-min.wav' },
-      { id: 2, name: 'Soft Sketch', src: 'Soft-Sketch.wav' },
+      { id: 2, name: 'Soft Sketch', src: 'Soft Sketch.wav' },
       { id: 3, name: 'Light Rain, Drone Bass (C)', src: 'Light-Rain_Drone-Bass_C.wav' },
       { id: 4, name: 'Soft Drone Bass (C)', src: 'Soft-Drone-Bass_C.wav' },
       { id: 5, name: 'Soft Flute (C)', src: 'Soft-Flute_C.wav' },
