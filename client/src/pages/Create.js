3d062702713e9eac63040410f7d46b30e22c0bf9
Jalyper <keaton.r.anderson@gmail.com>
2024-01-25 19:16:51 -0700

    changed how create page was getting userId

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -159,7 +159,7 @@ function Create() {
         return { status: 500 };
       }
 
-      return { status: responseBody.status, body: responseBody };
+      return { status: response.status, body: responseBody };
     };
 
     const userArrangementsData = {
